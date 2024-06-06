3cc470c728a1ebf8e2c1aaf77cbefebd4f3a5596
Eduardo Magno <edumagno@usp.br>
2024-06-06 10:33:34 -0300

    Update teste-publish.py

--- tests/teste-publish.py
+++ tests/teste-publish.py
@@ -8,6 +8,13 @@ python3 teste-publish.py
 with open("arquivo.txt", "r") as file:
     arquivo = file.read()
     mqttc.publish("teste", arquivo, qos=1)
+
+
+print(f"Lista de tópicos: {topicos}")
+topico = input("Digite o tópico: ").upper()
+if topico not in topicos:
+    print("[ERRO]: Tópico não encontrado")
+    exit()
 """
 
 topico = topicos[0]
