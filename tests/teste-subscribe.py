299624be293d425229e449cf6eccb30017534501
Eduardo Magno <edumagno@usp.br>
2024-06-06 10:20:53 -0300

    Update teste-subscribe.py

--- tests/teste-subscribe.py
+++ tests/teste-subscribe.py
@@ -3,5 +3,5 @@ from settings import *
 for i in topicos:
     mqttc.subscribe(i, qos=1)
     mqttc.on_message = on_message
-    #mqttc.on_connect = on_connect
+#mqttc.on_connect = on_connect
 mqttc.loop_forever()
