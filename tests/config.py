af8a713650e72c6055d767c0b6843535eaa7c6eb
Eduardo Magno <edumagno@usp.br>
2024-06-10 12:58:44 -0300

    att

--- tests/config.py
+++ tests/config.py
@@ -18,13 +18,25 @@ def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     client.subscribe("$SYS/#")
 
-arquivo = os.path.join(os.path.dirname(__file__), "historico.txt")
+historico = os.path.join(os.path.dirname(__file__), "historico.txt")
+#log = os.path.join(os.path.dirname(__file__), "log.txt")
 
 def on_message(client, userdata, msg):
     message = msg.payload.decode()
     topic = msg.topic
     date = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-    texto = f"{topic} [{date}]: {message}"
-    print(texto)
-    with open(arquivo, "a") as file:
-        print(texto, file=file)
+    text = f"{topic} [{date}]: {message}"
+    id_msg = get_id(msg)
+    print(text)
+    with open(historico, "a") as file:
+        print(f"{id_msg} = {text}", file=file)
+
+def get_id(msg):
+    id_topico = topicos.index(msg.topic)
+    num_msg = 1
+    with open(historico, "r") as file:
+        for line in file:
+            if msg.topic in line:
+                num_msg += 1
+    id = f"{id_topico}.{num_msg}"
+    return id
